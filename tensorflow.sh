6da817a9a607329d38f832b072a08966bc028a96
lukaskurz <lukaskurz5@gmail.com>
2018-10-20 21:53:24 +0200

    added compose file

--- tensorflow.sh
+++ tensorflow.sh
@@ -1,2 +1,2 @@
-docker run --runtime=nvidia -it -p 8888:8888 tensorflow/tensorflow:latest-gpu  # Start a Jupyter notebook server
-#docker run --runtime=nvidia -it -p 8888:8888 -v ~/tensorflow:/home/tensorflow -w /home/tensorflow tensorflow/tensorflow:latest-gpu
+docker run --runtime=nvidia -it -p 8888:8888 tensorflow/tensorflow:latest-gpu-py3 # Start a Jupyter notebook server
+#docker run --runtime=nvidia -it -p 8888:8888 -v ~/.:/home/tensorflow -w /home/tensorflow tensorflow/tensorflow:latest-gpu-py3
