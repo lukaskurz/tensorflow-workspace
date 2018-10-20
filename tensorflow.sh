7f120152a2d0a11dfae4f4866de6ceecc9d3549a
lukaskurz <lukaskurz5@gmail.com>
2018-10-20 22:15:47 +0200

    removed unnecessary shell script

--- tensorflow.sh
+++
@@ -1,2 +0,0 @@
-docker run --runtime=nvidia -it -p 8888:8888 tensorflow/tensorflow:latest-gpu-py3 # Start a Jupyter notebook server
-#docker run --runtime=nvidia -it -p 8888:8888 -v ~/.:/home/tensorflow -w /home/tensorflow tensorflow/tensorflow:latest-gpu-py3
