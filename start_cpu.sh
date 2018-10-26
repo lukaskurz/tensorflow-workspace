9abfba1d6d32e076d418eadf7d62684ee3f6f472
lukaskurz <lukaskurz5@gmail.com>
2018-10-26 11:48:41 +0200

    changed separation to work better

--- start_cpu.sh
+++ start_cpu.sh
@@ -1,3 +1,10 @@
-docker-compose up -d tensorflow
+docker run \
+    -it \
+    -p 8888:8888 \
+    -v ./jupyter_notebook_config.py:/root/.jupyter/jupyter_notebook_config.py \
+    -v ./src:/notebooks \
+    --name tensorflow-workspace-cpu \
+    tensorflow/tensorflow:latest-py3
 google-chrome http://localhost:8888/?token=c41b83686a46bc94ae03adddbae8e5270d6a1a6ce3796746
-docker-compose down
+docker rm --force tensorflow-workspace-cpu
+
